49dcc9f4e56b9dda46cbdd2af79a1739c58cbd5b
Falanga <stefano.falanga94@gmail.com>
2018-04-30 19:29:02 -0400

    ..

---
+++ app/data/friends.js
@@ -0,0 +1,74 @@
+
+var friendsArray = [
+	{
+	  "name":"Donald Trump",
+	  "photo":"http://www.lawyersgunsmoneyblog.com/wp-content/uploads/2015/12/nbc-fires-donald-trump-after-he-calls-mexicans-rapists-and-drug-runners-1024x768.jpg",
+	  "sex":"male",
+	  "scores":[
+	     5,
+	     1,
+	     4,
+	     4,
+	     5,
+	     1,
+	     2,
+	     5,
+	     4,
+	     1
+	   ]
+	},
+	{
+	  "name":"Vladimir Putin",
+	  "photo":"https://qph.fs.quoracdn.net/main-qimg-8735596e1412bb23a5f5f7eea14c4fc0.webp",
+	  "sex":"female",
+	  "scores":[
+	     1,
+	     1,
+	     1,
+	     1,
+	     1,
+	     1,
+	     1,
+	     1,
+	     1,
+	     1
+	   ]
+	},
+	{
+	  "name":"Rick Sanchez",
+	  "photo":"https://cdn.costumewall.com/wp-content/uploads/2017/01/rick-sanchez.jpg",
+	  "sex":"female",
+	  "scores":[
+	     4,
+	     5,
+	     3,
+	     3,
+	     4,
+	     5,
+	     1,
+	     4,
+	     3,
+	     5
+	   ]
+	},
+	{
+	  "name":"Morty Smith",
+	  "photo":"https://static.giantbomb.com/uploads/scale_small/0/9517/2816097-tumblr_n45cr8dmj61ty0km0o7_1280.png",
+	  "sex":"male",
+	  "scores":[
+	     2,
+	     3,
+	     1,
+	     1,
+	     2,
+	     3,
+	     4,
+	     2,
+	     1,
+	     3
+	   ]
+	}
+];
+
+// Set up table to be accessible to other files using require.
+module.exports = friendsArray;
